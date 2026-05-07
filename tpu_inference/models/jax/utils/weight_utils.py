536bb815516a3746a61442a18b3839e180ebaa74
Hitesh Yadav <hitesy@google.com>
2026-05-07 09:19:11 +0000

    Preventing default permute for layers.

--- tpu_inference/models/jax/utils/weight_utils.py
+++ tpu_inference/models/jax/utils/weight_utils.py
@@ -867,7 +867,7 @@ class JaxAutoWeightsLoader(AutoWeightsLoader):
                         N, H, D = param.value.shape
                         reshape_dims = (D, N, H)
                         permute_dims = (1, 2, 0)
-                elif "down_proj.weight" in name:
+                elif any(substr in name for substr in ["down_proj.weight", "gate_proj.weight", "up_proj.weight"]):
                     permute_dims = (0, 1)
                 elif "embed_tokens.weight" in name:
                     permute_dims = (0, 1)
